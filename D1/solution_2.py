fef23d7c029277fefe166cfe6b503e448aad6789
Peric01 <129906545+Peric01@users.noreply.github.com>
2025-12-11 18:10:51 +0100

    removed debug 2nd puzzle

--- D1/solution_2.py
+++ D1/solution_2.py
@@ -15,29 +15,22 @@ with open("D1\input.txt", "r") as file:
             temp_value = value - amount
             if value == 0:
                 value = 100 - amount
-                print(value, " ", solution)
             elif temp_value == 0:
                 value = temp_value
                 solution += 1
-                print(value, " ", solution)
             elif temp_value < 0:
                 value = temp_value + 100
                 solution += 1
-                print(value, " ", solution)
             else:
                 value = temp_value
-                print(value, " ", solution)
         else:
             temp_value = value + amount
             if temp_value == 100:
                 value = 0
                 solution += 1
-                print(value, " ", solution)
             elif temp_value > 100:
                 value = temp_value - 100
                 solution += 1
-                print(value, " ", solution)
             else:
                 value = temp_value
-                print(value, " ", solution)
 print(solution)
